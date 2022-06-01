003c559206b1798bc82170505bbf526e1a0e6e94
WilliamsBD8 <wsbonilladiaz@gmail.com>
2022-06-01 16:00:03 -0500

    junio 1 - arreglos app y database

--- app/Config/App.php
+++ app/Config/App.php
@@ -21,8 +21,8 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'http://localhost:8081/'; 
-	// public $baseURL = 'https://cespidh.com/cms_prueba_v2/public/';
+	// public $baseURL = 'http://localhost:8081/'; 
+	public $baseURL = 'https://cespidh.com/cms_prueba_v2/public/';
 
 	/*
 	|--------------------------------------------------------------------------
